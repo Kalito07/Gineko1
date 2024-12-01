c97e6b75ab50911241f58f3bc9fdc786f2fa421d
Kali Tihomirova <test@w3schools.com>
2024-12-01 16:54:15 +0200

    Auth with socials

--- app/auth/register.tsx
+++ app/auth/register.tsx
@@ -6,12 +6,12 @@ import Logo from "@/components/auth/Logo";
 import InputField from "@/components/auth/InputField";
 import translations from './../../translations.json';
 import SubmitButton from "@/components/auth/SubmitButton";
-import SocialButtonsContainer from "@/components/auth/SocialButtonsContainer";
 import AuthTitle from "@/components/auth/AuthTitle";
 import Or from "@/components/auth/Or";
 import AuthLayout from "@/layouts/_authLayout";
 import { useRouter } from "expo-router";
 import { useSignUp } from '@clerk/clerk-expo';
+import SocialButtonsContainer from "@/components/auth/SocialButtonsContainer";
 
 export default function RegisterScreen({ navigation }: { navigation: NavigationProp<RootStackParamList> }) {
     const { isLoaded, signUp, setActive } = useSignUp();
@@ -36,7 +36,7 @@ export default function RegisterScreen({ navigation }: { navigation: NavigationP
 
             await signUp.prepareEmailAddressVerification({ strategy: 'email_code' });
             setPendingVerification(true);
-            setErrorMessages([]); // Clear errors if successful
+            setErrorMessages([]);
         } catch (err: any) {
             console.error(JSON.stringify(err, null, 2));
             if (err.errors) {
@@ -103,7 +103,7 @@ export default function RegisterScreen({ navigation }: { navigation: NavigationP
                     <Or text={translations.auth.orRegister} />
 
                     <View style={{ marginBottom: 24 }}>
-                         {/*<SocialButtonsContainer />*/}
+                         <SocialButtonsContainer />
                     </View>
                 </>
             ) : (
